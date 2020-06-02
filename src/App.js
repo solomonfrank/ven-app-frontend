cac07d16b380f6dfa7e47a5f572fd1a674378025
Rock Enwerem <rock@cashboxng.com>
2020-06-02 12:30:57 +0100

    setting up folder structure and basic config

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import PublicRoute from "./BaseLayout/PublicRoute";
+
+import "./App.css";
+
+import Home from "./Pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <PublicRoute path="/" exact component={Home} />
+    </Router>
   );
 }
 
